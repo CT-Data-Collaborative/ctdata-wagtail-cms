4eaf5a352e3ad920e538630f2bb5b6545b8b184f
Sasha Cuerda <sasha.cuerda@gmail.com>
2017-04-20 17:27:20 -0400

    using data attributes to store graphic id which will be passed in to the scrolling activate function to dispatch the correct event

--- ctdata/static/ctdata/js/scrolly_graphics.js
+++ ctdata/static/ctdata/js/scrolly_graphics.js
@@ -1,4 +1,5 @@
 function initialize() {
+  console.log("initialized");
   var scroll = scroller()
     .container(d3.select('#scrolly-story'));
 
@@ -7,9 +8,15 @@ function initialize() {
 
   // setup event handling
   scroll.on('active', function (index) {
+
     // highlight current step text
     d3.selectAll('.step')
-      .style('opacity', function (d, i) { return i === index ? 1 : 0.1; });
+        .style('opacity', function (d, i) { return i === index ? 1 : 0.1; })
+        .each(function(d, i) {
+          if (i === index) {
+            console.log(d3.select(this).select('div[data-graphic-id]').attr('data-graphic-id'));
+          }
+        });
 
   });
 }
